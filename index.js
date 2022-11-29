66c978feb8c298299c1c0c1b13588e52902a31fa
stellagitonga <stellanyamburagitonga@gmail.com>
2022-11-29 21:17:23 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+function findMatching(drivers, received) {
+    
+    const result = drivers.filter(passedDriver=> passedDriver.toUpperCase() === received.toUpperCase());  
+    if(result){
+        return result
+    }
+
+}
+
+function fuzzyMatch(drivers, received) {
+
+    const result = drivers.filter(passedDriver=>passedDriver.toLowerCase().indexOf(received.toLowerCase())===0)
+    if(result){
+        return result
+        
+    }
+
+}
+function matchName(drivers, received) {
+
+    const result = drivers.filter(passedDriver=> passedDriver.name === received);
+    if(result){
+        return result
+    }
+    
+}
+ 
